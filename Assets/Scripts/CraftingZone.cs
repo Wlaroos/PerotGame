3ad433c8ba25567cba7a030a13bf5667219d2e42
Mikey <mikeybess27@gmail.com>
2025-11-07 02:35:07 -0600

    v4 Scene touchups

--- Assets/Scripts/CraftingZone.cs
+++ Assets/Scripts/CraftingZone.cs
@@ -81,7 +81,7 @@ public class CraftingZone : MonoBehaviour
                 }
                 else
                 {
-                    Debug.Log($"Crafting started: press {_requiredPresses - _currentPresses} more time(s).");
+                    //Debug.Log($"Crafting started: press {_requiredPresses - _currentPresses} more time(s).");
                 }
             }
             else
